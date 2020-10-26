873a0a835badef6290fcd8f9917588171b6d3b0f
nickolas benakis <nikolaos.benakis@workable.com>
2020-10-26 20:25:25 +0200

    Add sass

--- config/plugins.js
+++ config/plugins.js
@@ -2,17 +2,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const isAnalyzer = require('./isAnalyzer');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const path = require('path');
 
 const htmlGenerator = new HtmlWebpackPlugin({
-    title: 'my webpack app',
-    template: 'template/index.html'
+  title: 'my webpack app',
+  template: 'template/index.html',
 });
 
 const bundleAnalyzer = isAnalyzer() ? new BundleAnalyzerPlugin() : () => {};
 
-const manifestPlugin = new ManifestPlugin();
-
-const plugins = [manifestPlugin, htmlGenerator, bundleAnalyzer];
+const plugins = [
+  new ManifestPlugin(),
+  htmlGenerator,
+  bundleAnalyzer,
+  new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+];
 
 module.exports = plugins;
--- config/rules.js
+++ config/rules.js
@@ -9,6 +9,22 @@ const rules = [
         },
       },
     },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('node-sass')
+          }
+        }
+      ]
+    },
   ]
 
 module.exports = rules;
